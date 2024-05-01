2dde0cf2d3c84db374d25d015549f3c1d743d8fd
akasha-rec <uspira@naver.com>
2024-05-01 18:04:13 +0900

    모델2 방식 게시판 만들기

---
+++ MustHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
@@ -0,0 +1,144 @@
+package model2.mvcboard;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import common.JDBConnect;
+
+public class MVCBoardDAO extends JDBConnect {
+	public MVCBoardDAO() {
+		super();
+	}
+	
+	//검색 조건에 맞는 게시물의 개수 반환
+	public int selectCount(Map<String, Object> map) {
+		int totalCount = 0;
+		
+		String query = "SELECT COUNT(*) FROM mvcboard";
+		if (map.get("searchWord") != null) {
+			query += " WHERE " + map.get("searchField") + " "
+					+ " LIKE '%" + map.get("searchWord") + "%'";
+		}
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			rs.next();
+			totalCount = rs.getInt(1); //검색된 게시물 개수 저장
+		} catch (Exception e) {
+			System.out.println("게시물 카운트 중 예외 발생");
+			e.printStackTrace();
+		}
+		return totalCount; //게시물 개수를 서블릿으로 반환
+	}
+	
+	//검색 조건에 맞는 게시물 목록을 반환(페이징 기능 지원)
+	public List<MVCBoardDTO> selectListPage(Map<String, Object> map) {
+		List<MVCBoardDTO> board = new Vector<MVCBoardDTO>();
+		String query = "SELECT * FROM mvcboard";
+		if (map.get("searchWord") != null) {
+			query += " WHERE " + map.get("searchField") + " "
+					+ " LIKE '%" + map.get("searchWord") + "%' ";
+		}
+		query += "		ORDER BY idx DESC limit ?,?";
+		
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setInt(1, (int)map.get("start"));
+			psmt.setInt(2, (int)map.get("pageSize"));
+			
+			rs = psmt.executeQuery();
+			
+			while (rs.next()) {
+				
+				MVCBoardDTO dto = new MVCBoardDTO();
+				
+				dto.setIdx(rs.getString("idx"));
+				dto.setName(rs.getString("name"));
+				dto.setTitle(rs.getString("title"));
+				dto.setContent(rs.getString("content"));
+				dto.setPostdate(rs.getDate("postdate"));
+				dto.setOfile(rs.getString("ofile"));
+				dto.setSfile(rs.getString("sfile"));
+				dto.setDowncount(rs.getString("downcount"));
+				dto.setPass(rs.getString("pass"));
+				dto.setVisitcount(rs.getString("visitcount"));
+				
+				board.add(dto);
+			}
+		}
+		catch (Exception e) {
+			System.out.println("게시물 조회 중 예외 발생");
+			e.printStackTrace();
+		}
+		return board;
+	}
+	
+	//게시글 데이터를 받아 DB에 추가(업로드 지원)
+	public int insertWrite(MVCBoardDTO dto) {
+		int result = 0;
+		try {
+			String query = "INSERT INTO mvcboard ( "
+					+ " name, title, content, ofile, sfile, pass) "
+					+ " VALUES ( "
+					+ " ?, ?, ?, ?, ?, ?)";
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, dto.getName());
+			psmt.setString(2, dto.getTitle());
+			psmt.setString(3, dto.getContent());
+			psmt.setString(4, dto.getOfile());
+			psmt.setString(5, dto.getSfile());
+			psmt.setString(6, dto.getPass());
+			
+			result = psmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("게시물 입력 중 예외 발생");
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	//주어진 일련번호에 해당하는 게시물을 DTO에 담아 반환
+	public MVCBoardDTO selectView(String idx) {
+		MVCBoardDTO dto = new MVCBoardDTO();
+		String query = "SELECT * FROM mvcboard WHERE idx=?";
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, idx);
+			rs = psmt.executeQuery();
+			
+			if (rs.next()) {
+				dto.setIdx(rs.getString(1));
+				dto.setName(rs.getString(2));
+				dto.setTitle(rs.getString(3));
+				dto.setContent(rs.getString(4));
+				dto.setPostdate(rs.getDate(5));
+				dto.setOfile(rs.getString(6));
+				dto.setSfile(rs.getString(7));
+				dto.setDowncount(rs.getString(8));
+				dto.setPass(rs.getString(9));
+				dto.setVisitcount(rs.getString(10));
+			}
+		} catch (Exception e) {
+			System.out.println("게시물 상세보기 중 예외 발생");
+			e.printStackTrace();
+		}
+		return dto;
+	}
+	
+	//주어진 일련번호에 해당하는 게시물의 조회수 1 증가
+	public void updateVisitCount(String idx) {
+		String query = "UPDATE mvcboard SET "
+				+ " visitcount = visitcount+1 "
+				+ " WHERE idx=?";
+		
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, idx);
+			psmt.executeQuery();
+		} catch (Exception e) {
+			System.out.println("게시물 조회수 증가 중 예외 발생");
+			e.printStackTrace();
+		}
+	}
+}
--- MustHaveJSP/src/main/java/utils/JSFunction.java
+++ MustHaveJSP/src/main/java/utils/JSFunction.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.jsp.JspWriter;
 
 public class JSFunction {
@@ -15,6 +18,7 @@ public class JSFunction {
 		}
 		catch (Exception e) {}
 	}
+	
 	// 메시지 알림창을 띄운 후 이전 페이지로 돌아갑니다.
 	public static void alertBack(String msg, JspWriter out) {
 		try {
@@ -24,6 +28,35 @@ public class JSFunction {
 					+ "    history.back();"
 					+ "</script>";
 			out.println(script);
+		}
+		catch (Exception e) {}
+	}
+	//메시지 알림창을 띄운 후 명시한 url로 이동
+	public static void alertLocation(HttpServletResponse resp,String msg, String url) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+					+ "<script>"
+					+ "    alert('" + msg + "');"
+					+ "    history.back();"
+					+ "</script>";
+			writer.print(script);
+		}
+		catch (Exception e) {}
+	}
+	
+	// 메시지 알림창을 띄운 후 이전 페이지로 돌아갑니다.
+	public static void alertBack(HttpServletResponse resp, String msg) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+					+ "<script>"
+					+ "    alert('" + msg + "');"
+					+ "    history.back();"
+					+ "</script>";
+			writer.print(script);
 		}
 		catch (Exception e) {}
 	}
